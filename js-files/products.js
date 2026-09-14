08ab0ba9892f66b2a42753d34e035af794feb825
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2026-09-14 13:30:37 +1000

    feat: product lists
    adding logos and website official links -- will finish this up today

--- js-files/products.js
+++ js-files/products.js
@@ -5,62 +5,98 @@ const products = [
         title: "Procreate",
         desc: "",
         link: "https://procreate.com/",
-        type_category: ["drawing"],
-        price_category: "one-time-payment"
+        type_category: ["all", "drawing", "animation"],
+        price_category: ["all", "one-time-payment"]
     },
     {
         id: 2,
-        img: '',
-        title: "Ibis Paint",
+        img: './HTML-graphics/ibispaint-logo.webp',
+        title: "Ibis Paint X",
         desc: "",
-        link: "",
-        type_category: ["drawing"],
-        price_category: "free"
+        link: "https://ibispaint.com/about.jsp?lang=en",
+        type_category: ["all", "drawing"],
+        price_category: ["all", "free"]
     },
     {
         id: 3,
-        img: '',
+        img: './HTML-graphics/clipstudiopaint_logo.png',
         title: "Clip Studio Paint",
         desc: "",
-        link: "",
-        type_category: ["drawing"],
-        price_category: ""
+        link: "https://www.clipstudio.net/en/",
+        type_category: ["all", "drawing", "animation"],
+        price_category: ["all", "one-time-payment", "subscription-based"]
     },
     {
         id: 4,
-        img: '',
+        img: './HTML-graphics/adobefresco_logo.webp',
         title: "Adobe Fresco",
         desc: "",
-        link: "",
-        type_category: [""],
-        price_category: ""
+        link: "https://www.adobe.com/au/products/fresco.html",
+        type_category: ["all", "drawing", "animation"],
+        price_category: ["all", "free"]
     },
     {
         id: 5,
-        img: '',
+        img: './HTML-graphics/krita_logo.webp',
         title: "Krita",
         desc: "",
-        link: "",
-        type_category: ["drawing", "animation"],
-        price_category: "free"
+        link: "https://krita.org/en/",
+        type_category: ["all", "drawing", "animation"],
+        price_category: ["all", "free"]
     },
     {
         id: 6,
         img: '',
-        title: "Clip Studio Paint ",
+        title: "Colour Adobe",
         desc: "",
-        link: "",
-        type_category: [""],
-        price_category: ""
+        link: "https://color.adobe.com/create/color-wheel",
+        type_category: ["all", ""],
+        price_category: ["all", ""]
     },
     {
         id: 3,
         img: '',
-        title: "Clip Studio Paint ",
+        title: "Coolors",
         desc: "",
-        link: "",
-        type_category: [""],
-        price_category: ""
+        link: "https://coolors.co/",
+        type_category: ["all", ],
+        price_category: ["all", ""]
+    },
+    {
+        id: 3,
+        img: '',
+        title: "Wiggly Paint",
+        desc: "",
+        link: "https://internet-janitor.itch.io/wigglypaint",
+        type_category: ["all", ],
+        price_category: ["all", ""]
+    },
+    {
+        id: 3,
+        img: '',
+        title: "Effect.App",
+        desc: "",
+        link: "https://effect.app/",
+        type_category: ["all", ],
+        price_category: ["all", ""]
+    },
+    {
+        id: 3,
+        img: '',
+        title: "Salvaged.NU",
+        desc: "",
+        link: "https://salvaged.nu/",
+        type_category: ["all", ],
+        price_category: ["all", ""]
+    },
+    {
+        id: 3,
+        img: '',
+        title: "Character Design References",
+        desc: "",
+        link: "https://characterdesignreferences.com/",
+        type_category: ["all", "croquis-reference"],
+        price_category: ["all", ""]
     },
 
 ]
