8d7e43a04f63239279ac034709abdb116cd8dfc9
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2026-09-20 00:22:26 +1000

    feat: product descriptions + logos
    i put in descriptions and logos for a bunch of products but i am still yet to complete all the products... i will do this first thing tomorrow mornign

--- js-files/products.js
+++ js-files/products.js
@@ -119,11 +119,11 @@ const products = [
     {
         id: 14,
         title: "Alight Motion",
-        desc: "",
-        img: '',
-        link: "",
-        type_category: ["animation, image-video-editing"],
-        price_category: ["free"]
+        desc: "A professional motion design app available on mobile and desktop for creating professional-quality animation, motion-graphics, video effects and video editing. The free version allows for core animation and video editing tools such as keyframes, multiple layers and a limited selection of visual effects, but adds a watermark when exporting. Many advanced effects, filters, elements and fonts can only be accessed through the premium version.",
+        img: './HTML-graphics/alightmotion-logo.png',
+        link: "https://support.alightmotion.com/hc/en-us",
+        type_category: ["animation", "image-video-editing"],
+        price_category: ["free", "subscription-based"]
     },
     {
         id: 15,
@@ -209,7 +209,7 @@ const products = [
     {
         id: 24,
         title: "Toyhouse",
-        desc: "",
+        desc: "Toyhouse is a commissions ",
         img: './HTML-graphics/toyhouse-logo.webp',
         link: "https://toyhou.se/",
         type_category: ["commissions"],
@@ -218,7 +218,7 @@ const products = [
     {
         id: 25,
         title: "Ctrl Paint",
-        desc: "Large library of comprehensive video tutorials for digital art and illustration in general with slow demos and explanations by professional artist and youtuber Ctrl+Paint.",
+        desc: "CTRL Paint is a large digital library of comprehensive video tutorials for digital art and illustration in general. It is popular among beginner to intermediate artists for its slow demos and explanations by professional artist and youtuber Ctrl+Paint. It also offers a shop of purchaseable tutorials with more comprehensive guides, resources and video demonstrations.",
         img: './HTML-graphics/ctrlpaint-logo.jpg',
         link: "https://toyhou.se/",
         type_category: ["other"],
@@ -227,7 +227,7 @@ const products = [
     {
         id: 26,
         title: "Flim",
-        desc: "",
+        desc: "Flim is an AI powered visual search engine that allows users to browse through over 2 million scene snapshots of films, series, ads and music videos. It has a highly advanced filter system where artists can find specific references based on filters such as camera angles, moods, colour palettes, genre, year and artists.",
         img: './HTML-graphics/flim-logo.png',
         link: "https://app.flim.ai/",
         type_category: ["other"],
@@ -236,7 +236,7 @@ const products = [
     {
         id: 27,
         title: "Heavy Paint",
-        desc: "",
+        desc: "Digital painting application with a minimalistic, distraction-free interface that encourages expressive and experimental illustrations. Offers unique colour dynamics through its inbuilt colour jitter and is considered the ideal tool for painting studies and concept art.",
         img: './HTML-graphics/heavypaint-logo.png',
         link: "https://www.heavypaint.com/hp3-3/",
         type_category: ["drawing"],
@@ -245,16 +245,16 @@ const products = [
     {
         id: 28,
         title: "Online Image Editor",
-        desc: "",
+        desc: "Free image editing website that allows you to resize, crop, merge, blend and overlay images. It was popularised among artists for its vast range of animated gif glitter effects that can be used on your illustrations!",
         img: './HTML-graphics/onlineimageeditor-logo.png',
-        link: "https://toyhou.se/",
-        type_category: ["image-video-editing"],
+        link: "https://www.online-image-editor.com/",
+        type_category: ["image-video-editing", "textures-graphics"],
         price_category: ["free"]
     },
     {
         id: 29,
         title: "Frutiger Aero Archive",
-        desc: "",
+        desc: "An online archive dedicated to providing artists and creatives with 4000+ graphics, wallpapers, videos and icons inspired by the popularised Frutiger Aero aesthetic. It has a very distinct early 2000s vibe and can be used for creating mixed media illustrations and animations.",
         img: './HTML-graphics/frutigeraeroarchive-logo.png',
         link: "https://frutigeraeroarchive.org/resources",
         type_category: ["textures-graphics"],
@@ -263,7 +263,7 @@ const products = [
     {
         id: 30,
         title: "Texture Labs",
-        desc: "",
+        desc: "A popular online graphics resource for finding free, original high-resolution textures for art and design. Features a diverse range of textures filtered by categories such as grunge, fabric, film, half-tones and weird science, as well as an extensive collection of tutorials for how to make unique effects and textures by yourself.",
         img: './HTML-graphics/texturelabs-logo.jpeg',
         link: "https://texturelabs.org/",
         type_category: ["textures-graphics"],
@@ -272,7 +272,7 @@ const products = [
     {
         id: 31,
         title: "Cosmos",
-        desc: "",
+        desc: "Cosmos is AI-powered visual discovery app designed specifically for artists and like creatives as an ad-free and curated alternative to Pinterest. It allows for the saving of elements and graphics in personal libraries called Clusters, where your feed is based purely on inspiration and taste rather than an element's number of likes, comments or saves.",
         img: './HTML-graphics/cosmos-logo.png',
         link: "https://www.cosmos.so/explore/art",
         type_category: ["textures-graphics", "other"],
@@ -281,8 +281,8 @@ const products = [
     {
         id: 32,
         title: "Film Grab",
-        desc: "",
-        img: './HTML-graphics/filmgrab-logo.png',
+        desc: "Film Grab is vast, extensive collection of scene snapshots from films made single-handedly by film-maker and cinematographer Dawn Coffrey. Allows for browsing alphabetically and by specific artists and categories, where these snapshots can be used for character, lighting and composition studies and inspiration.",
+        img: './HTML-graphics/filmgrab-logo.jpeg',
         link: "https://film-grab.com/",
         type_category: ["croquis-reference", "other"],
         price_category: ["free"]
@@ -298,9 +298,9 @@ const products = [
     },
     {
         id: 34,
-        title: "Live2d",
-        desc: "",
-        img: './HTML-graphics/',
+        title: "Live2D",
+        desc: "Live2D is an industry standard tool for animating/rigging 2D illustrations into fully-angled models without the need of converting to 3D. It supports a layered artwork where creators can set key poses for actions like blinking and smiling that can be used for creating character rigs for artists and streamers.",
+        img: './HTML-graphics/live2d-logo.jpeg',
         link: "https://www.live2d.com/en/",
         type_category: ["animation"],
         price_category: ["free", "subscription-based"]
@@ -308,17 +308,17 @@ const products = [
     {
         id: 35,
         title: "Rough Animator",
-        desc: "",
-        img: './HTML-graphics/',
-        link: "",
+        desc: "A mobile animation software that allows for powerful traditional hand-drawn animation capabilities including timeline, onion skinning, unlimited layers, importing audios/videos and creating custom brushes.",
+        img: './HTML-graphics/roughanimator-logo.jpg',
+        link: "https://www.roughanimator.com/",
         type_category: ["animation"],
-        price_category: ["free"]
+        price_category: ["one-time-payment"]
     },
     {
         id: 36,
         title: "Cloud Stop Motion",
-        desc: "",
-        img: './HTML-graphics/',
+        desc: "User-friendly animation tool for creating stop motion animation through frame by frame capture without installing software or losing access to work. Has features such as a zoomable timeline, audio tools and text and title inserts.",
+        img: './HTML-graphics/cloudstopmotion-logo.png',
         link: "https://app.cloudstopmotion.com/v2/animate/dashboard/projects",
         type_category: ["animation"],
         price_category: ["free"]
@@ -326,8 +326,8 @@ const products = [
     {
         id: 37,
         title: "Bluestacks",
-        desc: "",
-        img: './HTML-graphics/',
+        desc: "Bluestacks is an Android emulator that lets you to run mobile apps and games on Windows PC or Mac, essentially allowing you to run art applications and tools that are usually unavailable on desktop.",
+        img: './HTML-graphics/bluestacks-logo.webp',
         link: "https://www.bluestacks.com/",
         type_category: ["other"],
         price_category: ["free"]
@@ -335,63 +335,63 @@ const products = [
     {
         id: 38,
         title: "Capcut",
-        desc: "",
-        img: './HTML-graphics/',
+        desc: "Capcut is a popular, free to download photo and video editing tool powered by AI. It supports editing features such as multi-layer editing, an advanced timeline, speed templates, as well as a vast library of transitions, filters and effects. It is most convenient for its in-built background remover, text to speech and caption generating tools.",
+        img: './HTML-graphics/capcut-logo.png',
         link: "https://www.capcut.com/",
         type_category: ["image-video-editing"],
-        price_category: ["free"]
+        price_category: ["free", "subscription-based"]
     },
     {
         id: 39,
         title: "OpenToonz",
-        desc: "",
-        img: './HTML-graphics/',
-        link: "",
+        desc: "A powerful open-source 2D animation software that supports hybrid layers, traditional and rig animation and global colour palettes. This application also comes with 100+ inbuilt effects and SDK, as well as a specialised scanning tool to import traditional drawings straight to digital.",
+        img: './HTML-graphics/opentoonz-logo.webp',
+        link: "https://opentoonz.org/",
         type_category: ["animation"],
         price_category: ["free"]
     },
     {
         id: 40,
         title: "Tahoma2D",
-        desc: "",
-        img: './HTML-graphics/',
-        link: "",
+        desc: "An open-source 2D and stop-motion animation software based on OpenToonz with a more streamlined and user-friendly interface. However, its simplified workspace still includes advanced features such as persective grid, symmetry and referenced fills.",
+        img: './HTML-graphics/tahoma2d-logo.png',
+        link: "https://tahoma2d.org/",
         type_category: ["animation"],
         price_category: ["free"]
     },
     {
         id: 41,
         title: "Blender3D Grease Pencil",
-        desc: "",
-        img: './HTML-graphics/',
-        link: "",
-        type_category: ["animation"],
+        desc: "Grease pencil is a unique Blender object that accepts drawing information and places them on 3D points, allowing for 2D art and animation to be created in a 3D space. This is highly beneficial for creating cut-out animation, motion graphics and animations with dynamic perspectives or hand-drawn effects with 3D lighting and environments.",
+        img: './HTML-graphics/blender-logo.webp',
+        link: "https://docs.blender.org/manual/en/latest/grease_pencil/introduction.html#quick-start",
+        type_category: ["drawing", "animation"],
         price_category: ["free"]
     },
     {
         id: 42,
         title: "Toonsquid",
-        desc: "",
-        img: './HTML-graphics/',
-        link: "",
-        type_category: ["animation"],
-        price_category: ["free"]
+        desc: "2D art and animation app that supports both hand-drawn frame by frame and keyframe animation with a large customisable library of pixel and vector brushes. Also includes tools such as adding animation effects to layers through bones and meshes, re-using animation, creating camera layers, vector shapes and custom colour palettes.",
+        img: './HTML-graphics/toonsquid-logo.png',
+        link: "https://toonsquid.com/",
+        type_category: ["drawing", "animation"],
+        price_category: ["one-time-payment"]
     },
     {
         id: 43,
         title: "Artistree",
-        desc: "",
-        img: 'https://artistree.io/',
-        link: "",
+        desc: "An online platform designed specifically for artists to manage commissions where artists can receive 100% of their advertised commission price with no platform fee for the artist. The Artistree Dashboard allows for formal queue tracking and accepting commission requests that ultimately removes the need for messy direct messages.",
+        img: './HTML-graphics/artistree-logo.jpg',
+        link: "https://artistree.io/",
         type_category: ["commissions"],
         price_category: ["free"]
     },
     {
         id: 44,
         title: "Ko-Fi",
-        desc: "",
-        img: 'https://ko-fi.com/',
-        link: "",
+        desc: "Ko-Fi is a free to use creator-monetisation platform used by artists for posting commissions and phsyical goods/merch where artists can host monthly memberships, list custom work, set availability/slots and interact with buyers. Note that this site has a 5% platform fee for all commissions.",
+        img: './HTML-graphics/kofi-logo.avif',
+        link: "https://ko-fi.com/",
         type_category: ["commissions"],
         price_category: ["free"]
     },
